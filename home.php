d397478b70d3cece2167973c9a6f7b7106c52fa0
kunal12899 <kunal12899@gmail.com>
2016-08-03 12:10:04 -0500

    file change

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 $firstname = $_POST['fname'];
 $lastname = $_POST['lname'];
 
-$to = "kunal12899@gmail..com";
+$to = "kunal12899@gmail.com";
 $subject = "Hello World";
 $message = "Firstname: $firstname \n\n Lastname: $lastname";
 $from = "kunal12899@gmail.com";
