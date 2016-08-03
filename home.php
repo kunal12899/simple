164bb4437a2f8706e7925403d5001724beda4f66
kunal12899 <kunal12899@gmail.com>
2016-08-03 12:04:59 -0500

    change

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ $lastname = $_POST['lname'];
 $to = "kunal12899@gmail..com";
 $subject = "Hello World";
 $message = "Firstname: $firstname \n\n Lastname: $lastname";
-$from = "sender@example.com";
+$from = "kunal12899@gmail.com";
 $headers = "From:" . $from;
 mail($to,$subject,$message,$headers);
 echo "Mail Sent.";
